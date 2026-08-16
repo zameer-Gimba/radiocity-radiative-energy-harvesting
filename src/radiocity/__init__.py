b6272427037585af1f3fccc2fa9996093ccdac02
Zameer_GM <mibrahimgimba@gmail.com>
2026-08-16 15:52:37 +0100

    Add radiocity package initializer

---
+++ src/radiocity/__init__.py
@@ -0,0 +1,5 @@
+"""Radiocity radiative-energy harvesting models."""
+
+from .model import RadiationChannel, SystemParameters, simulate_step
+
+__all__ = ["RadiationChannel", "SystemParameters", "simulate_step"]
