fb403bed6b8730c0474d4b0606223042a6da0f13
Zameer_GM <mibrahimgimba@gmail.com>
2026-08-28 18:12:01 +0100

    Expose integrated AI optimization API

--- src/radiocity/__init__.py
+++ src/radiocity/__init__.py
@@ -1,5 +1,12 @@
 """Radiocity radiative-energy harvesting models."""
 
+from .ai_optimization import OptimizationResult, optimize_spectral_channel
 from .model import RadiationChannel, SystemParameters, simulate_step
 
-__all__ = ["RadiationChannel", "SystemParameters", "simulate_step"]
+__all__ = [
+    "OptimizationResult",
+    "RadiationChannel",
+    "SystemParameters",
+    "optimize_spectral_channel",
+    "simulate_step",
+]
