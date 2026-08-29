dbadac3a6d98c2029f90efd32e838a5f58e1b3df
Zameer_GM <mibrahimgimba@gmail.com>
2026-08-29 09:25:50 +0100

    Expose adaptive AI optimization bridge

--- src/radiocity/__init__.py
+++ src/radiocity/__init__.py
@@ -1,12 +1,17 @@
 """Radiocity radiative-energy harvesting models."""
 
+from .adaptive_controller import ControlLimits, allocate_capture, optimize_and_allocate, rank_channels
 from .ai_optimization import OptimizationResult, optimize_spectral_channel
 from .model import RadiationChannel, SystemParameters, simulate_step
 
 __all__ = [
+    "ControlLimits",
     "OptimizationResult",
     "RadiationChannel",
     "SystemParameters",
+    "allocate_capture",
+    "optimize_and_allocate",
     "optimize_spectral_channel",
+    "rank_channels",
     "simulate_step",
 ]
