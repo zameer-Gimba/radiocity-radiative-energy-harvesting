c897a38717addf2c533b0586063ebc472e8209fe
Zameer_GM <mibrahimgimba@gmail.com>
2026-08-29 09:37:39 +0100

    Expose multi-source harvesting and energy repository

--- src/radiocity/__init__.py
+++ src/radiocity/__init__.py
@@ -2,14 +2,20 @@
 
 from .adaptive_controller import ControlLimits, allocate_capture, optimize_and_allocate, rank_channels
 from .ai_optimization import OptimizationResult, optimize_spectral_channel
+from .emr_model import EMRChannel
+from .energy_repository import EnergyRepository
 from .model import RadiationChannel, SystemParameters, simulate_step
+from .multi_source import harvest_multi_source
 
 __all__ = [
     "ControlLimits",
+    "EMRChannel",
+    "EnergyRepository",
     "OptimizationResult",
     "RadiationChannel",
     "SystemParameters",
     "allocate_capture",
+    "harvest_multi_source",
     "optimize_and_allocate",
     "optimize_spectral_channel",
     "rank_channels",
